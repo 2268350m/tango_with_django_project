7ec9fcc3ef1851037501c032f7ffcef9e7c22f4c
Stuart <2268350m@student.gla.ac.uk>
2018-01-26 07:13:10 +0000

    Chapter 5 (W/o Exercises)

--- rango/urls.py
+++ rango/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^about/', views.about, name = 'about'),
-]
+] 
--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     html = "Rango says hey there partner!" + "  " '<a href="/rango/about/">about</a>'
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, "rango/index.html", context=context_dict)
     return HttpResponse(html)
 
 def about(request):
-    html = "Rango says here is the about page." + "  " + '<a href="/rango/">index</a>'
+    html = "This tutorial has been put together by Stuart McMillan" + "  " + '<a href="/rango/">index</a>'
+    context_d = {"boldmessage": "Template using boldmessage"}
+    return render(request, "rango/about.html", context=context_d)
     return HttpResponse(html)
