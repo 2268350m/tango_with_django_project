fe1eadc643d40e887018c410ba9afc570d98ac04
Stuart <2268350m@student.gla.ac.uk>
2018-01-26 09:53:46 +0000

    Fixed Media file

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,13 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    #url(r'^rango/about/', views.about, name='about',
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-]
-    #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
