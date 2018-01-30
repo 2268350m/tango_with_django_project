68a1f2d68ff8d0c1aa82022843fedead68e63d4d
Stuart <2268350m@student.gla.ac.uk>
2018-01-30 21:39:39 +0000

    (Chapter 5 tests okay)

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
-
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     html = "Rango says hey there partner!" + "  " '<a href="/rango/about/">about</a>'
-    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, "rango/index.html", context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
     return HttpResponse(html)
 
 def about(request):
@@ -14,3 +16,16 @@ def about(request):
     context_d = {"boldmessage": "This tutorial has been put together by Stuart McMillan"}
     return render(request, "rango/about.html", context=context_d)
     return HttpResponse(html)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
