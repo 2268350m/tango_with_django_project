fb3ebb144bb8af06180e6af0081001c1f4b4bb7b
Stuart <2268350m@student.gla.ac.uk>
2018-02-06 08:49:49 +0000

    (Chapter 7. Tests passed.)

--- rango/urls.py
+++ rango/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
     url(r'^about/', views.about, name = 'about'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^page/(?P<page_name_slug>[\w\-]+)/$',
-        views.show_category, name='show_page')
+        views.show_category, name='show_page'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+        views.add_page, name='add_page'),
 ] 
